e9ac6a4d568542cf30c7f6e644cc1a56249768f1
Dataniel31 <alexdspe02@gmail.com>
2024-08-30 18:58:21 -0500

    Initial commit

---
+++ src/main/java/com/youdesign/YouDesign/YouDesignApplication.java
@@ -0,0 +1,13 @@
+package com.youdesign.YouDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youdesign/YouDesign/YouDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youdesign.YouDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
