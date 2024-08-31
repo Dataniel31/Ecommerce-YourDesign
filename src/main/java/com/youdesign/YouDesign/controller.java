a43f41b3e6e7c50e443d09bd850c13a000b13a7f
Dataniel31 <alexdspe02@gmail.com>
2024-08-30 19:15:53 -0500

    add controller and indexhtml

---
+++ src/main/java/com/youdesign/YouDesign/controller.java
@@ -0,0 +1,13 @@
+package com.youdesign.YouDesign;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class controller {
+    @GetMapping("/index")
+    public String MostrarInicio(){
+        return "index";
+    }
+}
