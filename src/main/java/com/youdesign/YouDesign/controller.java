61e534ef6d44af76e4419c39cb2fc249cb32f51a
Dataniel31 <alexdspe02@gmail.com>
2024-09-03 18:30:41 -0500

    done

--- src/main/java/com/youdesign/YouDesign/controller.java
+++ src/main/java/com/youdesign/YouDesign/controller.java
@@ -16,4 +16,34 @@ public class controller {
     public String login( ) {
         return "login";
     }
+
+    @GetMapping("/lreclamaciones")
+    public String lreclamaciones(Model model) {
+        model.addAttribute("pageTitle", "Libro de Reclamaciones");
+        return "libro_reclamaciones";
+    }
+
+    @GetMapping("/terminosycondiciones")
+    public String terminosycondiciones(Model model) {
+        model.addAttribute("pageTitle", "Terminos y Condiciones");
+        return "terminos_condiciones";
+    }
+
+    @GetMapping("/politicaprivacidad")
+    public String politicaprivacidad(Model model) {
+        model.addAttribute("pageTitle", "Politicas de Privacidad");
+        return "politica_privacidad";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros(Model model) {
+        model.addAttribute("pageTitle", "Sobre Nosotros");
+        return "sobre_nosotros";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto(Model model) {
+        model.addAttribute("pageTitle", "Contactanos");
+        return "contacto";
+    }
 }
