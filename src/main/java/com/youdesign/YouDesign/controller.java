c66d36ad3b507cec752aac8868d9738fd762fc34
Ivonne RP <yamilleivonne77@gmail.com>
2024-09-03 18:11:36 -0500

    he mapeado las  4 paginas que han creado es decir ya se puede modificar y ver el resultado. -el css carrusel.css no funciona por mas que lo añadi al index.html. en controller añadir los getmapping. falta el css de la pagina , que sea responsive y tambien rellenar las paginas , poner imagenes de la ropa que puso angelo en el prototipo.

--- src/main/java/com/youdesign/YouDesign/controller.java
+++ src/main/java/com/youdesign/YouDesign/controller.java
@@ -16,4 +16,17 @@ public class controller {
     public String login( ) {
         return "login";
     }
+
+    @GetMapping("/sobre_nosotros")
+    public String verNosotros() {
+        return "sobre_nosotros"; // Debe coincidir con el nombre del archivo HTML en la carpeta templates
+    }
+    @GetMapping("/productos")
+    public String verProductos() {
+        return "productos"; // Debe coincidir con el nombre del archivo HTML en la carpeta templates
+    }
+    @GetMapping("/contacto")
+    public String verContacto() {
+        return "contacto"; // Debe coincidir con el nombre del archivo HTML en la carpeta templates
+    }
 }
