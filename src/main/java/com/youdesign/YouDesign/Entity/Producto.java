1a5dd7f278ba1ac624b5dd1fe4075e6fcfef661f
Ivonne RP <yamilleivonne77@gmail.com>
2024-10-11 11:42:49 -0500

    cambios en applicationproperties , html

--- src/main/java/com/youdesign/YouDesign/Entity/Producto.java
+++ src/main/java/com/youdesign/YouDesign/Entity/Producto.java
@@ -1,6 +1,4 @@
 package com.youdesign.YouDesign.Entity;
-
-
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,7 +7,15 @@ import lombok.Data;
 @Table(name="productos")
 
 public class Producto {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id_producto;
+private String id_categoria;
+private String id_marca;
+private String color;
+private double precio;
+private int cantidad;
+private String imagen;
 
 
 }
