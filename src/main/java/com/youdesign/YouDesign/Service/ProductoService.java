7b3e642ac8c4c861b613f47621f257e667fe2a92
Alvaro <ap19371937@gmail.com>
2024-10-21 16:24:29 -0500

    Mejora de Productos

--- src/main/java/com/youdesign/YouDesign/Service/ProductoService.java
+++ src/main/java/com/youdesign/YouDesign/Service/ProductoService.java
@@ -1,31 +1,14 @@
 package com.youdesign.YouDesign.Service;
 
-import com.youdesign.YouDesign.Entity.Productos;
-import com.youdesign.YouDesign.Repository.ProductoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.youdesign.YouDesign.Dto.Productodto;
+import com.youdesign.YouDesign.Entity.Categoria;
+import com.youdesign.YouDesign.Entity.Marca;
+import com.youdesign.YouDesign.Entity.Producto;
 
 import java.util.List;
 
-@Service
-public class ProductoService {
-
-    @Autowired
-    private ProductoRepository productoRepository;
-
-    public List<Productos> listarTodos() {
-        return productoRepository.findAll();
-    }
-
-    public void save(Productos producto) {
-        productoRepository.save(producto);
-    }
-
-    public Productos obtenerProductoPorId(Long id) {
-        return productoRepository.findById(id).orElse(null);
-    }
-
-    public void eliminarProducto(Long id) {
-        productoRepository.deleteById(id);
-    }
+public interface ProductoService{
+    Producto save(Productodto productodto, Marca marca, Categoria categoria);
+    Producto update(Long id, Productodto productodto, Marca marca, Categoria categoria);
+    void delete(Long id_producto);
 }
